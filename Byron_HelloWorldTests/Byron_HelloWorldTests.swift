97c0170c0859ebdd425ac30d0675b8d1e9239b61
Byron Alfad <byronluc@byrons-macbook-pro.local>
2018-10-05 15:06:44 +0100

    Initial Commit

---
+++ Byron_HelloWorldTests/Byron_HelloWorldTests.swift
@@ -0,0 +1,34 @@
+//
+//  Byron_HelloWorldTests.swift
+//  Byron_HelloWorldTests
+//
+//  Created by Byron Alfad on 05/10/2018.
+//  Copyright © 2018 Byron Alfad. All rights reserved.
+//
+
+import XCTest
+@testable import Byron_HelloWorld
+
+class Byron_HelloWorldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
